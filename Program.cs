901696e275e58d1947445e77a539060b6e089a65
varshagandham <1210716153@gitam.in>
2020-07-13 20:36:40 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prog3_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a * b;       
+            b = a / b;      
+            a = a / b;   
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
